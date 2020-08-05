b5359bd95456f3a5bf257e936627ff8e6524f7f4
Luis <LVNUNEZ@entel.cl>
2020-08-05 10:57:12 -0400

    Grafico mejorado

--- graficCPU_1.js
+++ graficCPU_1.js
@@ -1,21 +1,23 @@
 new Morris.Line({
   // ID of the element in which to draw the chart.
-  element: 'myfirstchart2',
+  element: 'graficadecpu1',
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-    { año: '2008', value: 3 },
-    { año: '2010', value: 5  },
-    { año: '2012', value: 7}
+    { year: '2008', value: 3 },
+    { year: '2009', value: 3.5 },
+    { year: '2010', value: 5  },
+    { year: '2011', value: 5.4  },
+    { year: '2012', value: 7}
   ],
   // The name of the data record attribute that contains x-values.
-  xkey: 'año',
+  xkey: 'year',
   // A list of names of data record attributes that contain y-values.
   ykeys: ['value'],
   lineWidth: 1.5,
   // Labels for the ykeys -- will be displayed when you hover over the
   // chart.
-  labels: ['RAM'],
+  labels: ['CPU'],
   resize: true,
   responsive: true,
   maintainAspectRatio: false,
--- graficCPU_2.js
+++ graficCPU_2.js
@@ -1,23 +1,21 @@
 new Morris.Line({
   // ID of the element in which to draw the chart.
-  element: 'myfirstchart',
+  element: 'graficadecpu2',
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-    { year: '2008', value: 3 },
-    { year: '2009', value: 3.5 },
-    { year: '2010', value: 5  },
-    { year: '2011', value: 5.4  },
-    { year: '2012', value: 7}
+    { año: '2008', value: 3 },
+    { año: '2010', value: 5  },
+    { año: '2012', value: 7}
   ],
   // The name of the data record attribute that contains x-values.
-  xkey: 'year',
+  xkey: 'año',
   // A list of names of data record attributes that contain y-values.
   ykeys: ['value'],
   lineWidth: 1.5,
   // Labels for the ykeys -- will be displayed when you hover over the
   // chart.
-  labels: ['CPU'],
+  labels: ['RAM'],
   resize: true,
   responsive: true,
   maintainAspectRatio: false,
